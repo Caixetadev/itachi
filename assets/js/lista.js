d905f5fb2c38700a49b6c26b9bc23da57d5e9e44
Caixeta <caixetadev@gmail.com>
2021-11-04 08:56:13 -0300

    iframes

---
+++ assets/js/lista.js
@@ -0,0 +1,11 @@
+const skills = [
+    {
+        name: '<iframe width="560" height="315" src="https://www.youtube.com/embed/_0BydQTDymw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    },
+    {
+        name: '<iframe width="560" height="315" src="https://www.youtube.com/embed/IBwyFK6VqWo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    },
+    {
+        name: '<iframe width="560" height="315" src="https://www.youtube.com/embed/oeqNjdiWP6M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    }
+]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector('.modal')
 
+
 function closeModal() {
   modal.style.display = 'none'
 }
@@ -7,17 +8,23 @@ function closeModal() {
 const button = document.querySelectorAll('.modalOpen')
 
 button.forEach((b) => {
+  const index = b.getAttribute('data-id')
+  const { name } = skills[index]
 
   b.addEventListener('click', (event) => {
     event.preventDefault()
     modal.style.display = 'flex'
+    modal.innerHTML = `
+    <button class="close" onclick="closeModal()">X</button>
+    ${name}
+
+    `
   })
 
 })
 
-// const btn = document.querySelector('button')
-// const audio = document.querySelector('audio')
 
-// btn.addEventListener('click', () => {
-//   audio.pause
-// })
+/* const audio = document.querySelector('audio')
+window.onload = function music() {
+  audio.play()
+} */
